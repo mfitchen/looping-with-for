7bf48d451f8f9440887059f30d4a455897cf3f74
Marty Fitchen and Noah Yasskin <marty@>
2016-06-07 13:56:03 -0700

    add a working form to input a number and give the factorial value for it

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,23 @@
 
 // Everything below this line is user interface (or front-end) logic:
 
+
+// Factorial
 $(document).ready(function() {
+  $("button#factorialButton").click(function(event) {
+    var factorialInput = $("input#factorialInput").val();
+    var factorialParse = parseInt(factorialInput);
+    var factorialParse = factorialParse.toFixed(0);
+    var factorialTotal = 1;
 
-//var vowelsArray = ["a", "e", "i", "o", "u"]
-  var puzzleString = "You miss 100% of the shots you don't take. Wayne Gretzky";
-  var newPuzzleArray = puzzleString.split("");
-
-  for (var index = 0; index < newPuzzleArray.length; index += 1) {
-    if (newPuzzleArray[index] === "a" || newPuzzleArray[index] === "e" || newPuzzleArray[index] === "i" || newPuzzleArray[index] === "o" || newPuzzleArray[index] === "u") {
-      newPuzzleArray[index] = "-"
+    for (var factorial = factorialParse; factorial > 0; factorial--) {
+    	factorialTotal *= factorial;
+      //alert(total);
+      //console.log(factorialTotal);
     };
-  };
-  console.log(newPuzzleArray);
 
-  var newPuzzleString = newPuzzleArray.join("");
-  $(".puzzle-text").text(newPuzzleString);
-  $("#puzzle-guess").show();
-
-  $("button.btn").click(function() {
-    $(".quote").text(puzzleString);
-    $("#puzzle-output").show();
+    event.preventDefault();
+    $(".factorial").text(factorialTotal);
+    $("#factorialOutput").show();
   });
-
-// alert(newPuzzleString);
-// display#puzzle-guess
-//
-// display#puzzle-output
-
-  // event.preventDefault();
 });
-
-//newPuzzleString = puzzleString.replace(vowelsArray[index], "-");
