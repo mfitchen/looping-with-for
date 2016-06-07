e6644438a2f521f491625fcc91123dd24d6c7abf
Marty Fitchen and Noah Yasskin <noah@>
2016-06-07 15:27:48 -0700

    add a fully functioning for loop that checks for palindromes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
 // Business (or back-end) logic:
-
-
 // Everything below this line is user interface (or front-end) logic:
+
+// Palindromes
+$(document).ready(function() {
+  $("button#palindromeButton").click(function(event) {
+    var palindromeString = $("input#palindromeInput").val();
+    var palindromeString = palindromeString.toUpperCase();
+    var palindromeArray = palindromeString.split(" ");
+    // var palindromeArray = palindromeString.split(" ", ".", ",", "!");
+    var palindromeString = palindromeArray.join("");
+    var palindromeArray = palindromeString.split("");
+    console.log(palindromeArray);
+    var palindromeArrayReversed = [];
+
+    for (var index = (palindromeArray.length) - 1; index >= 0; index -= 1) {
+      palindromeArrayReversed.push(palindromeArray[index]);
+    };
+
+    var palindromeTruth = true;
+
+    for (var index = 0; index < palindromeArray.length; index += 1) {
+      if (palindromeArray[index] != palindromeArrayReversed[index]) {
+        var palindromeTruth = false;
+      };
+    };
+
+    if (palindromeTruth === false) {
+      $(".notPalindrome").text("not ");
+    } else {
+      $(".notPalindrome").text("");
+    };
+
+    $("#inputPalindrome").toggle();
+    $("#outputPalindrome").toggle();
+    event.preventDefault();
+  });
+});
+
 
 
 // Factorial
